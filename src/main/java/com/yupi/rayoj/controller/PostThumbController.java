8ab44f0ab12d2031fe01967eebed794c75c8485b
Ray Rao <sesamefoxx@outlook.com>
2024-03-07 16:38:17 +0800

    feat: 项目模板初始化 1. 项目架构搭建 2. 数据库，Redis等常用工具初始化 3. 基本的增删改查接口

---
+++ src/main/java/com/yupi/rayoj/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.yupi.rayoj.controller;
+
+import com.yupi.rayoj.common.BaseResponse;
+import com.yupi.rayoj.common.ErrorCode;
+import com.yupi.rayoj.common.ResultUtils;
+import com.yupi.rayoj.exception.BusinessException;
+import com.yupi.rayoj.model.dto.postthumb.PostThumbAddRequest;
+import com.yupi.rayoj.model.entity.User;
+import com.yupi.rayoj.service.PostThumbService;
+import com.yupi.rayoj.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
